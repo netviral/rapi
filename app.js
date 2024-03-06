7b95b8b8417860f04a9e97e21db2407244b54b40
Ibrahim Khalil <ibrahimkhalil@Ibrahims-MacBook-Air.local>
2024-03-06 08:10:58 +0530

    second

--- app.js
+++ app.js
@@ -8,28 +8,30 @@ app.listen(port, () => {
     console.log("Listening on port " + port);
 });
 allowed = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
 app.get("/:id",async function(req,res){
 var command = 'aplay';
     try {
         if(allowed.includes(req.params.id)){
-            res.send("done");
-            // await sound.play(req.params.id+".wav"
+            res.redirect("/");
+            await sound.play(req.params.id+".wav");
             command += " "+req.params.id+".wav";
-	    console.log(command);
             // Execute the command
-            exec(command, (error, stdout, stderr) => {
-                if (error) {
-                console.error(`Error executing the command: ${error.message}`);
-                return;
-                }
-                if (stderr) {
-                console.error(`Command stderr: ${stderr}`);
-                return;
-                }
-                console.log(`Command stdout: ${stdout}`);
-            });
+            // exec(command, (error, stdout, stderr) => {
+            //     if (error) {
+            //     console.error(`Error executing the command: ${error.message}`);
+            //     return;
+            //     }
+            //     if (stderr) {
+            //     console.error(`Command stderr: ${stderr}`);
+            //     return;
+            //     }
+            //     console.log(`Command stdout: ${stdout}`);
+            // });
         }else{
-            res.send("no note");
+            res.sendStatus(404);
         }
       } catch (error) {
         console.error(error);
